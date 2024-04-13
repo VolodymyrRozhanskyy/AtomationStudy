d8ff985849eac229cc3ef118f206b2c02546247e
Volodymyr.Rozhanskyy <it.rial.lviv@gmail.com>
2024-04-14 00:16:20 +0300

    fix webdriver

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
@@ -12,9 +9,7 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+    driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
-
     driver.quit()
-
