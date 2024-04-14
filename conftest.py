06caf78b8dc67d8a33989dfc43b3126900d4e8e6
Volodymyr.Rozhanskyy <it.rial.lviv@gmail.com>
2024-04-14 14:02:03 +0300

    chrome driver init refactor

--- conftest.py
+++ conftest.py
@@ -1,15 +1,16 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
-    options = Options()
+    service = Service()
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=service, options=options)
     request.cls.driver = driver
     yield driver
-    driver.quit()
